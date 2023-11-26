48324a74f665ce3210949b67142421547ace80c6
Sammie <lythekiet333@gmail.com>
2023-11-26 20:55:56 +1030

    Added props interface
    * Added custom attributes to props interface
    * Rename the type of the file to tsx

---
+++ react-app/src/components/button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface ButtonProps {
+  onClick?: () => void;
+  className?: string;
+  value: string;
+  id?: string;
+}
+
+const Button = ({ onClick, className, value }: ButtonProps) => {
+  return (
+    <button onClick={onClick} className={className}>
+      {value}
+    </button>
+  );
+};
+export default Button;
