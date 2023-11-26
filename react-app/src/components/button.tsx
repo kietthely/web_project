d539d554b07462a4f34db7dc405f15729143f959
Sammie <lythekiet333@gmail.com>
2023-11-26 21:11:45 +1030

    Added pre-defined styles
    * Added custom inline styles with style prop is in-line CSS styles
    * Allows styles from className

--- react-app/src/components/button.tsx
+++ react-app/src/components/button.tsx
@@ -2,14 +2,30 @@ import React from "react";
 
 interface ButtonProps {
   onClick?: () => void;
+  style?: React.CSSProperties;
   className?: string;
   value: string;
   id?: string;
 }
+// predefined styles
+const defaultStyle = {
+  backgroundColor: "#007bff",
+  background: "linear-gradient(to right, #34ebdf, #34ebc6)",
+  color: "black",
+  border: "none",
+  padding: "10px 20px",
+  borderRadius: "6px",
+  cursor: "pointer",
+  transition: "background-color 0.3s ease",
+};
 
-const Button = ({ onClick, className, value }: ButtonProps) => {
+const Button = ({ onClick, className, style, value }: ButtonProps) => {
   return (
-    <button onClick={onClick} className={className}>
+    <button
+      onClick={onClick}
+      style={{ ...defaultStyle, ...style }}
+      className={className}
+    >
       {value}
     </button>
   );
