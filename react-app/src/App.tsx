a57a44d3c561166c483ac3c40a3e2503085696fa
Davin Ly <vinh.ly.softdev@gmail.com>
2023-11-29 22:39:04 +1030

    add sass and styles

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Button from './components/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,12 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <Button value='Custom Button' className='btn_custom'/>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
