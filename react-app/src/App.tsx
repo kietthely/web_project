6b8542a1267b168f9204cff879880b5afcfb7efa
Sammie <lythekiet333@gmail.com>
2023-12-12 05:20:12 +1030

    Update App
    Add ServicesListLayout for testing

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Button from './components/button'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import ServicesListLayout from "./components/ServicesListLayout";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -17,14 +17,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <Button value='Custom Button' className='btn_custom'/>
-      </div>
+      <ServicesListLayout />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
