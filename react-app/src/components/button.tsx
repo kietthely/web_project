52bdafc5d6bc736556283aa874b8c3ac3033913b
Sammie <lythekiet333@gmail.com>
2023-12-12 05:17:02 +1030

    Update button
    Add Link to the component

--- react-app/src/components/button.tsx
+++ react-app/src/components/button.tsx
@@ -1,16 +1,22 @@
+import { Link } from "react-router-dom";
 interface ButtonProps {
   onClick?: () => void;
   className?: string;
   value: string;
   id?: string;
+  to?: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return (
-    <button
-      onClick={props.onClick}
-      className={props.className}
-    >
+  // if navigation link is not provided then return only the button.
+  return props.to ? (
+    <Link to={props.to}>
+      <button onClick={props.onClick} className={props.className}>
+        {props.value}
+      </button>
+    </Link>
+  ) : (
+    <button onClick={props.onClick} className={props.className}>
       {props.value}
     </button>
   );
