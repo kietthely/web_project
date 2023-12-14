bb578a3c326b81793008fa82654582a640135392
Sammie <lythekiet333@gmail.com>
2023-12-14 16:45:53 +1030

    Update ServicesListLayout
    Add more cards to the layout

--- react-app/src/components/ServicesListLayout.tsx
+++ react-app/src/components/ServicesListLayout.tsx
@@ -2,22 +2,44 @@ import Button from "./Button";
 import Card from "./Card";
 const ServicesListLayout = () => {
   return (
-    <div className="card-container">
-      <Card className="card">
+    <div className="card-layout-service">
+      <Card className="card-service">
         <Button
           value="Custom button"
           className="btn_custom"
           to="/Services/MergePDFs"
         />
       </Card>
-      <Card className="card">
+      <Card className="card-service">
         <Button
           value="Custom button"
           className="btn_custom"
           to="/Services/MergePDFs"
         />
       </Card>
-      <Card className="card">
+      <Card className="card-service">
+        <Button
+          value="Custom button"
+          className="btn_custom"
+          to="/Services/PDFtoDocx"
+        />
+      </Card>
+
+      <Card className="card-service">
+        <Button
+          value="Custom button"
+          className="btn_custom"
+          to="/Services/MergePDFs"
+        />
+      </Card>
+      <Card className="card-service">
+        <Button
+          value="Custom button"
+          className="btn_custom"
+          to="/Services/MergePDFs"
+        />
+      </Card>
+      <Card className="card-service">
         <Button
           value="Custom button"
           className="btn_custom"
