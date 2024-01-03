3472303794513f9f43b90276e110c3c371d8c494
Sammie <lythekiet333@gmail.com>
2024-01-03 20:23:19 +1030

    Add other features of the card
    Adds body, image, title, description, buttons to the card frame.

--- react-app/src/components/Card.tsx
+++ react-app/src/components/Card.tsx
@@ -1,9 +1,33 @@
+import React from "react";
+
 interface CardProps {
   className?: string;
   children?: React.ReactNode;
 }
-
-const Card = (props: CardProps) => {
-  return <div className={props.className}>{props.children}</div>;
+const truncate = (text: string, length: number) => {
+  return text.length > length ? text.substr(0, length - 1) + "..." : text;
 };
-export default Card;
+const Card = (props: CardProps) => {
+  return <div className={`card ${props.className}`}>{props.children}</div>;
+};
+
+const CardImg = (props: { src: string; alt?: string }) => {
+  return <img className="card-img-top" src={props.src} alt={props.alt} />;
+};
+
+const CardBody = (props: CardProps) => {
+  return <div className="card-body">{props.children}</div>;
+};
+
+const CardTitle = (props: CardProps) => {
+  return <h5 className="card-title">{props.children}</h5>;
+};
+
+const CardDescription = (props: CardProps) => {
+  return <p className="card-description">{props.children}</p>;
+};
+const CardActions = (props: CardProps) => {
+  return <div className={`card ${props.className}`}>{props.children}</div>;
+};
+
+export { Card, CardImg, CardBody, CardTitle, CardDescription, CardActions };
