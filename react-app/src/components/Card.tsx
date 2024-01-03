5436171ec919aef9db5c3941e118f2ddb7ed6f0a
Sammie <lythekiet333@gmail.com>
2024-01-03 21:58:10 +1030

    Add CardLinkProps and CardImgProps
    Add CardLinkProps and CardImgProps.
    Complete all features of the card

--- react-app/src/components/Card.tsx
+++ react-app/src/components/Card.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 interface CardProps {
   className?: string;
+  children?: React.ReactNode;
+}
+interface CardImgProps {
+  className?: string;
+  src: string;
+  alt?: string;
+}
+interface CardLinkProps {
+  to: string;
   children?: React.ReactNode;
 }
 const truncate = (text: string, length: number) => {
@@ -11,8 +20,14 @@ const Card = (props: CardProps) => {
   return <div className={`card ${props.className}`}>{props.children}</div>;
 };
 
-const CardImg = (props: { src: string; alt?: string }) => {
-  return <img className="card-img-top" src={props.src} alt={props.alt} />;
+const CardImg = (props: CardImgProps) => {
+  return (
+    <img
+      className={`card-img ${props.className}`}
+      src={props.src}
+      alt={props.alt}
+    />
+  );
 };
 
 const CardBody = (props: CardProps) => {
@@ -27,7 +42,20 @@ const CardDescription = (props: CardProps) => {
   return <p className="card-description">{props.children}</p>;
 };
 const CardActions = (props: CardProps) => {
-  return <div className={`card ${props.className}`}>{props.children}</div>;
+  return (
+    <div className={`card-actions ${props.className}`}>{props.children}</div>
+  );
+};
+const CardLink = (props: CardLinkProps) => {
+  return <Link to={props.to}>{props.children}</Link>;
 };
 
-export { Card, CardImg, CardBody, CardTitle, CardDescription, CardActions };
+export {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardDescription,
+  CardActions,
+  CardLink,
+};
